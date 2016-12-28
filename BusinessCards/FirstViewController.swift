ec138b96ebe52b0473e3a17fde759e6a9a466173
Ajani <ajani@Ajanis-iMac.local>
2016-12-28 07:57:57 -0800

    Initial Commit

---
+++ BusinessCards/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BusinessCards
+//
+//  Created by Ajani on 28/12/2016.
+//  Copyright © 2016 Ajani. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
