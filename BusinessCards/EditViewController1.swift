766d325aba06fa06400847acd52404f84f46b62e
Ajani <ajani@Ajanis-iMac.local>
2017-01-02 11:51:09 -0800

    sumtings2

--- BusinessCards/EditViewController1.swift
+++ BusinessCards/EditViewController1.swift
@@ -27,14 +27,53 @@ class EditViewController1: UIViewController {
     }
     
     public func initializeFields(textArray: [String]) {
+        var arrayCopy = textArray;
         
-        NameField.text = textArray[0]
-        EmailField.text = textArray[1]
-        NumberField.text = textArray[2]
-        WebAddrField.text = textArray[3]
+        
+            for j in 0...arrayCopy.count-1{
+                
+                if (textArray[j].components(separatedBy:" ").count==2){
+                    NameField.text = textArray[j]
+                }
+                if (isValidEmail(testStr: textArray[j])){
+                    EmailField.text = textArray[j]
+                }
+                if (validate(value: textArray[j])){
+                    NumberField.text = textArray[j]
+                }
+                if (verifyUrl(urlString: textArray[j])){
+                    WebAddrField.text = textArray[j]
+                }
+            }
+
 
     }
+    func verifyUrl (urlString: String?) -> Bool {
+        //Check for nil
+        if let urlString = urlString {
+            // create NSURL instance
+            if let url = NSURL(string: urlString) {
+                // check if your application can open the NSURL instance
+                return UIApplication.shared.canOpenURL(url as URL)
+            }
+        }
+        return false
+    }
     
+    func isValidEmail(testStr:String) -> Bool {
+    print("validate emilId: \(testStr)")
+    let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}"
+    let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+    let result = emailTest.evaluate(with: testStr)
+    return result
+    }
+    func validate(value: String) -> Bool {
+        let PHONE_REGEX = "^\\d{3}-\\d{3}-\\d{4}$"
+        let phoneTest = NSPredicate(format: "SELF MATCHES %@", PHONE_REGEX)
+        let result =  phoneTest.evaluate(with: value)
+        return result
+    }
+
 
 }
 
