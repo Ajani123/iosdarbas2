1b3955c77da334d8e5746f0f3306627c5ca6405b
Ajani <ajani@Ajanis-iMac.local>
2017-01-03 08:38:56 -0800

    App delegates pakeitimas, configai

---
+++ BusinessCards/Card+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Card+CoreDataClass.swift
+//  BusinessCards
+//
+//  Created by Ajani on 03/01/2017.
+//  Copyright © 2017 Ajani. All rights reserved.
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(Card)
+public class Card: NSManagedObject {
+
+}
---
+++ BusinessCards/Card+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  Card+CoreDataProperties.swift
+//  BusinessCards
+//
+//  Created by Ajani on 03/01/2017.
+//  Copyright © 2017 Ajani. All rights reserved.
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+
+extension Card {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Card> {
+        return NSFetchRequest<Card>(entityName: "Card");
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var email: String?
+    @NSManaged public var webaddr: String?
+    @NSManaged public var number: String?
+    @NSManaged public var isMyCard: Bool
+
+}
